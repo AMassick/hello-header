cbb45390c8cded8cbee54ef2142f6ab9f7febe32
adam massick <adam.massick@gmail.com>
2014-02-20 14:47:26 -0800

    version 1

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,16 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
+	// returns 0 to 'limit'
+	var limit = 2;
+	var numRand = Math.floor(Math.random()*limit);
+
+	var msg0 = 'Hello!';
+	var msg1 = 'Hola!';
+	var msg2 = 'Bonjour!';
+
+	$('h1').text('msg' + numRand);
+
 	console.log('Page Loaded. Lets Do this!');
 
 }); 
